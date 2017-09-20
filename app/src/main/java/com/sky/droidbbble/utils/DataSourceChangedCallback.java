f5180f69ed5c8e72b2ce55e6d795b8862732d9e6
baocheng <baocheng@skyworth.com>
2017-09-20 09:47:27 +0800

    add notes

--- app/src/main/java/com/sky/droidbbble/utils/DataSourceChangedCallback.java
+++ app/src/main/java/com/sky/droidbbble/utils/DataSourceChangedCallback.java
@@ -6,6 +6,10 @@ import android.support.v7.util.ListUpdateCallback;
  * Created by tonycheng on 2017/9/19.
  */
 
+/**
+ * 这个类主要用来判断下拉刷新时，现有的数据源对比最新从服务器获取的数据源是否发生了变化，
+ * 避免添加重复的数据。核心思想是使用 {@link android.support.v7.util.DiffUtil}
+ */
 public abstract class DataSourceChangedCallback implements ListUpdateCallback {
 
     public abstract void onDataSourceChanged();
